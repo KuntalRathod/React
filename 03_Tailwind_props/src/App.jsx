bdd91752c7832adefdb423d92083c503ed9ed947
KuntalRathod <nature.kr70@gmail.com>
2023-09-05 01:54:37 +0530

    Tailwind and Props

---
+++ 03_Tailwind_props/src/App.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/no-unknown-property */
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  let myObj = {
+    username: 'John',
+    age: 25,
+  }
+
+  return (
+    <>
+      <Card username="ChaiAurCode" btnText="click me" />
+      <Card username="Kuntal" />
+
+    </>
+  )
+}
+
+export default App
