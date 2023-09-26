268c293d2ced67a3aaab555714537dd076d124d1
KuntalRathod <nature.kr70@gmail.com>
2023-09-27 00:44:19 +0530

    configure redux toolkit done

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,14 +1,54 @@
-import './App.css'
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import './App.css';
+import { login, logout } from './store/authSlice';
+import {Footer} from './components';
+import { Header } from './components';
+import { Outlet } from 'react-router-dom';
+
+
+
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        }
+        else {
+          dispatch(logout())
+        }
+      }).catch
+      ((error) => {
+        console.log("App.jsx :: useEffect :: error", error)
+      })
+      .finally(()=>setLoading(false))
+
+  }, [])
+  
+
+  //console.log(import.meta.env.VITE_APPWRITE_URL);
  
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-scree flex-wrap content-between bg-slate-400 '>
+
+      <div className='w-full block'>
+        <Header />
+        <main>
+         TODO: <Outlet />
+        </main>
+        <Footer />
+
+      </div>
+  
+    </div>
+  ): null
 }
 
 export default App
--- 12_MegaBlog/src/appwrite/auth.js
+++ 12_MegaBlog/src/appwrite/auth.js
@@ -61,4 +61,4 @@ export class AuthService{
 
 const authService = new AuthService();
 
-export default authService();
+export default authService;
