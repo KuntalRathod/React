a8a7ab50abc6d943ea8d901b23e10048043c8b57
KuntalRathod <nature.kr70@gmail.com>
2023-09-29 21:03:41 +0530

    React hooks form in production done

--- 12_MegaBlog/src/components/Button.jsx
+++ 12_MegaBlog/src/components/Button.jsx
@@ -10,7 +10,7 @@ const Button = ({
 }) => {
   return (
       <button className={`px-4 py-2 rounded-lg 
-      ${bgColor} ${textColor} ${className}`} {...props}>
+      ${bgColor} ${textColor} ${className}`} {...props2}>
           {children}
       </button>
   )
